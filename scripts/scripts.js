e089912861e6b94d9e1a8dab94008b81996dc575
timwlee <timothyl@adobe.com>
2024-06-03 18:40:56 -0700

    Update header, hero, and footer styling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,11 +21,12 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
  */
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
+  const h4 = main.querySelector('h4');
   const picture = main.querySelector('picture');
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    section.append(buildBlock('hero', { elems: [picture, h1, h4] }));
     main.prepend(section);
   }
 }
